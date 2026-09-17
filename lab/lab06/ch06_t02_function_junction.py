f4ef1e96371927d75882ee4a4db876a03ac392ef
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-17 08:08:47 +0000

    [skip actions] lab/lab06/ch06_t02_function_junction.py

--- lab/lab06/ch06_t02_function_junction.py
+++ lab/lab06/ch06_t02_function_junction.py
@@ -1,8 +1,6 @@
 # Define your spam function starting on line 5. You
 # can leave the code on line 10 alone for now--we'll
 # explain it soon!
-
-
 # Define the spam function above this line.
 def  spam():
     print('Eggs!')
