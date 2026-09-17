ed45dd2cd612fe2f01578f99a3f96b71d4d21279
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-17 07:48:38 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -1,8 +1,9 @@
 pyg = 'ay'
+original = input('Enter a word:')
 word=original.lower()
 first=word[0]
 
-original = input('Enter a word:')
+
 
 if len(original) > 0 and original.isalpha():
     print(original)
