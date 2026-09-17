7afe7a678c46ae6bc5a107870c23358342f7c406
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-17 12:34:58 +0000

    [skip actions] lab/lab06/ch06_t19_builtin_functions.py

--- lab/lab06/ch06_t19_builtin_functions.py
+++ lab/lab06/ch06_t19_builtin_functions.py
@@ -1,5 +1,5 @@
 def distance_from_zero(kousaka:any):
-    if type(kousaka)=="<class'int'>" or type(kousaka)=="<class'float'>":
+    if type(kousaka)==int or type(kousaka)=="<class'float'>":
         return abs(kousaka)
     else:
         return 'nope'
