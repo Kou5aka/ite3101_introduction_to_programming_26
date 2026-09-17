f2a0b68574789b1fa3d65c255d5dc0a82729bf82
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-17 07:14:29 +0000

    lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -7,7 +7,7 @@ bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 bool_two = 1>2 and 2>3 or 'AAA' == 'AAA'
 
 # Make me false!
-bool_three = not False and 33>11
+bool_three = not False and 33<11
 
 # Make me true!
 bool_four = 11==11 and not False or '321'=='123'
