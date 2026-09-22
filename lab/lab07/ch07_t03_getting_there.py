61b2c0b46326f60b3ee9894a7ec9ca3be81bdff4
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-22 10:13:26 +0000

    lab/lab07/ch07_t03_getting_there.py

--- lab/lab07/ch07_t03_getting_there.py
+++ lab/lab07/ch07_t03_getting_there.py
@@ -4,3 +4,8 @@ def plane_ride_cost(city):
     if city=="Charlotte":
         return 183
     elif city=="Tampa":
+        return 220
+    elif city=="Pittsburgh":
+        return 222
+    elif city=="Los Angeles":
+        return 475
