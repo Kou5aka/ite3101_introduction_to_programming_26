06a5f9439d65e8fe93a8b38cc609a51777094805
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-22 10:39:49 +0000

    [skip actions] lab/lab07/ch07_t05_pull_it_tgt.py

--- lab/lab07/ch07_t05_pull_it_tgt.py
+++ lab/lab07/ch07_t05_pull_it_tgt.py
@@ -22,4 +22,4 @@ def rental_car_cost(days: int) -> int:
     return cost
 
 def trip_cost(city,days):
-    return rental_car_cost(days)+plane_ride_cost(city)
+    return rental_car_cost(days)+hotel_cost(nights)+plane_ride_cost(city)
