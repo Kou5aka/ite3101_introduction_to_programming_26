b20bf9271cd344bb62f1f874422f6d2fcd91e39b
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-24 08:39:42 +0000

    [skip actions] lab/lab08/ch08_t14_last_note.py

--- lab/lab08/ch08_t14_last_note.py
+++ lab/lab08/ch08_t14_last_note.py
@@ -7,7 +7,7 @@ inventory = {
 inventory['pocket'] = ['seashell', 'strange berry', 'lint']
 inventory['pocket'].sort()
 inventory['backpack']=inventory['pocket']
-backpack.remove('dragger')
+inventory[].remove('dragger')
 # Adding a key 'burlap bag' and assigning a list to it
 inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
 
