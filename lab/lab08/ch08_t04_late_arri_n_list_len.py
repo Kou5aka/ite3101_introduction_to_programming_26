b4ed0b5a14a51fb7e08fdb5ba381f3bcac0ac845
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-24 07:23:42 +0000

    [skip actions] lab/lab08/ch08_t04_late_arri_n_list_len.py

--- lab/lab08/ch08_t04_late_arri_n_list_len.py
+++ lab/lab08/ch08_t04_late_arri_n_list_len.py
@@ -6,7 +6,7 @@ suitcase.append("bathing suit")
 suitcase.append("T_shirt")
 suitcase.append("Jacket")
 
-list_length = 4  # Set this to the length of suitcase
+list_length = 3  # Set this to the length of suitcase
 
 print("There are %d items in the suitcase." % list_length)
 print(suitcase)
