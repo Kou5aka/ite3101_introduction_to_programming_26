d0cf61ee15c91665bfcb9aaf03d2d8d29c9c76af
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-24 07:41:04 +0000

    lab/lab08/ch08_t06_slicing_lists_n_strings.py

--- lab/lab08/ch08_t06_slicing_lists_n_strings.py
+++ lab/lab08/ch08_t06_slicing_lists_n_strings.py
@@ -4,7 +4,7 @@ animals = "catdogfrog"
 cat =animals[:3]
 
 # The fourth through sixth characters
-dog = animals[4:6]
+dog = animals[3:6]
 
 # From the seventh character to the end
 frog = animals[6:]
