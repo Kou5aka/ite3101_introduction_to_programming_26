47b1c6a0ffee704a9e962e0db2869fdcaa6e94fa
Kou5aka <156404147+Kou5aka@users.noreply.github.com>
2026-09-24 08:36:28 +0000

    [skip actions] lab/lab08/ch08_t14_last_note.py

--- lab/lab08/ch08_t14_last_note.py
+++ lab/lab08/ch08_t14_last_note.py
@@ -4,7 +4,7 @@ inventory = {
     'pouch': ['flint', 'twine', 'gemstone'],
     'backpack': ['xylophone', 'dagger', 'bedroll', 'bread loaf']
 }
-
+inventory[]
 # Adding a key 'burlap bag' and assigning a list to it
 inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
 
